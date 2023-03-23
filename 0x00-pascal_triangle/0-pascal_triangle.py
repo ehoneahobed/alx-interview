540933c0aef0404274c52ce3a0df9a36611f604a
ehoneahobed <ehoneahobed@hotmail.com>
2023-03-23 20:37:23 +0000

    Update the file

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A script to determine pascal's triangle for any number"""
 
+
 def pascal_triangle(n):
     """
     returns a list of lists of integers representing the Pascal’s triangle of n
