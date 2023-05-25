2848f3f026c6b65119f619ba6edbf959d7bafc97
ehoneahobed <ehoneahobed@hotmail.com>
2023-05-25 21:13:43 +0000

    Fix: pep8 styles

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """Island Perimeter - ALX Interview"""
 
+
 def check_val(x):
-        """_summary_
+    """_summary_
 
-        Args:
-            n (_type_): _description_
+    Args:
+        n (_type_): _description_
 
-        Returns:
-            _type_: _description_
-        """
-        if (x==0):
-            return 1
-        return 0
+    Returns:
+        _type_: _description_
+    """
+    if (x == 0):
+        return 1
+    return 0
+
 
 def island_perimeter(grid):
     """_summary_
@@ -20,7 +22,7 @@ def island_perimeter(grid):
     Args:
         grid (_type_): _description_
     """
-    
+
     row = len(grid)
     col = len(grid[0])
     assert (1 <= row and col <= 100), "length must be between 1 an 100"
@@ -29,7 +31,7 @@ def island_perimeter(grid):
     for i in range(row):
         for j in range(col):
             assert (grid[i][j] == 0) or (grid[i][j] == 1),\
-                                        "grid numbers must be 0 or 1"
+                "grid numbers must be 0 or 1"
             if grid[i][j] == 1:
                 if i-1 < 0:
                     x += 1
